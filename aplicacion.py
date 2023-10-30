d7e37a5eb1093737fe96a18b11edac50b3e6dd2a
davidveisaga <davidveisaga@gmail.com>
2023-10-30 20:13:21 -0300

    Integracion con jihja2

--- aplicacion.py
+++ aplicacion.py
@@ -6,7 +6,9 @@ app = Wsgiclass()
 
 @app.ruta("/home")
 def home(request, response):
-    response.text = "<h3>Pagina Home</h3>"
+    # response.text = "<h3>Pagina Home</h3>"
+    response.text = app.template(
+    "home.html", context={"title": "Pagina Principal", "user": "Pepe Sanchez"})
 
 @app.ruta("/otra")
 def otra(request, response):
